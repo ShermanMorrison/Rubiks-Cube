9f2c22715a2d8189733a2418874257a2847e6806
Jonathan Wilson <jdw7@rice.edu>
2015-10-17 17:01:58 -0500

    Made custom number spinner, Added Dimension ng controller methods
    The glyphicons need to have position: absolute, so that they are positioned relative to their containing positioned ancestor.
    
    The trick was to make sure that the nearest positioned ancestor is actually their parent (class inner-addon). Have to set inner-addon to have position: relative for the glyphicons to use it as the nearest positioned parent. If inner-addon is left to the default position: static, the glyphicons will treat the grandparent bootstrap col-sm-12 class as their nearest positioned parent.

--- scripts/cubeGUI-ng-module.js
+++ scripts/cubeGUI-ng-module.js
@@ -1,11 +1,19 @@
-
-
-$('.number-spinner').bootstrapNumber();
-
 var cubeGUI = angular.module('cubeGUI', []);
 
 cubeGUI.controller('cubeGUIController', function ($scope) {
     $scope.scramble = function(){ call_scramble(); };
     $scope.reset = function(){ call_reset(); };
     $scope.dimension = 3;
+    $scope.increment = function(){
+        if ($scope.dimension < 7){
+            $scope.dimension += 1;
+            call_reset($scope.dimension);
+        }
+    }
+    $scope.decrement = function(){
+        if ($scope.dimension > 3){
+            $scope.dimension -= 1;
+            call_reset($scope.dimension);
+        }
+    }
 });
